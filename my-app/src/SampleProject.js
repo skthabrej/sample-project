faa73777b9ffde683d1619f3c55961d4c9f84ac4
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-05 12:49:12 +0530

    sample project

---
+++ my-app/src/SampleProject.js
@@ -0,0 +1,14 @@
+import './SampleProject.css'
+import { Component } from "react";
+
+class SampleProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='head'>My sample project</h1>
+        <p className='para'>Myself Shaik Thabrej Basha</p>
+      </div>
+    )
+  }
+}
+export default SampleProject
